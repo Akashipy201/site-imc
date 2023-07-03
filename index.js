17e2b49d743a25dd8fb7e2e7760eed963387a668
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2023-07-02 22:31:09 -0300

    site-imc

---
+++ index.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector("button"); 
+const  peso =  document.querySelector(".peso"); 
+const altura = document.querySelector(".altura"); 
+const resposta = document.querySelector("#resposta"); 
+
+function IMC(valor1, valor2){
+
+    let valor_altura = parseFloat(valor1.replace(",", "."))
+    console.log(valor_altura)
+
+    let imc = (Math.floor(valor2)/(valor_altura*valor_altura)).toFixed(2)
+
+    return imc
+
+}
+
+
+
+
+btn.addEventListener("click", ()=>{
+    if(altura.value !== "" && peso.value !==""){
+        let valor = IMC(altura.value, peso.value)
+        resposta.innerHTML = valor
+
+    }
+    else{
+        alert("Porfavor, coloca todos os os dados que estão pedindo")
+    }
+})
