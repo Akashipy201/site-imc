8011cc7118e96fe315e3f5263811b271dda6379a
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2023-07-02 22:51:02 -0300

    js update 2

--- index.js
+++ index.js
@@ -6,26 +6,21 @@ const resposta = document.querySelector("#resposta");
 function IMC(valor1, valor2){
 
     let valor_altura = parseFloat(valor1.replace(",", "."))
-    console.log(valor_altura)
 
     let imc = (Math.floor(valor2)/(valor_altura*valor_altura)).toFixed(2)
-
-    return imc
-
+    return imc 
+    
 }
 
 
 
 
 btn.addEventListener("click", ()=>{
+
     if(altura.value !== "" && peso.value !==""){
         let valor = IMC(altura.value, peso.value)
-        if(valor!== NaN){
-            resposta.innerHTML = valor
-
-        }
-        else{
-            alert("Deu erro no sistema, porfavor tente novamente")
+        if(valor !=  "NaN"){
+            resposta.value = valor
         }
 
     }
