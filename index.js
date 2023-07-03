eb9bc204c514e460465b4a98313734cf90528868
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2023-07-03 18:06:23 -0300

    js e style update

--- index.js
+++ index.js
@@ -9,10 +9,27 @@ function IMC(valor1, valor2){
 
     let imc = (Math.floor(valor2)/(valor_altura*valor_altura)).toFixed(2)
     
-    return imc 
     
+    if (Number(imc)<=18.5){
+        return `O valor do seu IMC é ${imc.replace(".",",")}<br/>e está baixo do normal`
+    }
+    else if (Number(imc) > 18.6 && Number(imc) < 24.9){
+        return `O seu IMC é ${imc.replace(".",",")} <br/>e parabéns, o seu imc está normal ^-^`
+    }
+    
+    else if (Number(imc) > 25.0 && Number(imc) < 29.9){
+        return `O seu IMC é ${imc.replace(".",",")} <br/>Alerta de sobrepreso!!! começa a se cuidar...`
+    }
+    else if (Number(imc) > 30.0 && Number(imc) < 34.9){
+        return `O seu IMC é ${imc.replace(".",",")} <br/>Alerta de Obesidade grau 1 !!! começa a se cuidar...`
+    }
+    else if (Number(imc) > 35.0 && Number(imc) < 39.9){
+        return `O seu IMC é ${imc.replace(".",",")} <br/>Alerta de Obesidade grau 2 !!! começa a se cuidar...`
+    }
+    else if (Number(imc) > 40.0){
+        return `O seu IMC é ${imc.replace(".",",")} <br/>Alerta de Obesidade grau 3 !!!<br/> Aqui o sinal é vermelho, com forte probabilidade de já existirem doenças muito graves associadas. O tratamento deve ser ainda mais urgente.`
+    }
 }
-
 
 
 
